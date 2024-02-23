be155d31be7934de3be37109b246f1eef43c37b6
Femi Omotayo <omotayooluwafemi66@gmail.com>
2024-02-23 22:46:04 +0100

    authenticating with bcrypt, passport, jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
-  imports: [PostsModule, DatabaseModule],
+  imports: [PostsModule, DatabaseModule, UsersModule, AuthenticationModule],
   controllers: [AppController],
   providers: [AppService],
 })
