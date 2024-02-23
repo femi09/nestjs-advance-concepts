ff347ba2695dc4ee33f0fc41f4e192e44d468045
Femi Omotayo <omotayooluwafemi66@gmail.com>
2024-02-23 21:52:54 +0100

    setting up postgres database with typeorm and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
